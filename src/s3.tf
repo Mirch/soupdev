84cfe1e5fc3d0f45effa8a541e0dd51d732a0295
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-03-10 22:02:01 +0200

    Add S3 resources

---
+++ src/s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "suppdev-client" {
+  bucket = "suppdev-client"
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "suppdev-client-web-config" {
+  bucket = aws_s3_bucket.suppdev-client.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "null_resource" "upload-website" {
+  provisioner "local-exec" {
+    command = format("cd ./client && npm build && aws s3 sync ./client/build s3://%s", aws_s3_bucket.suppdev-client.bucket)
+  }
+}
