d937ab9d0c17b513ffbe3f23b05598b3349940b2
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-03-11 13:16:11 +0200

    Another configuration

--- src/main.tf
+++ src/main.tf
@@ -25,4 +25,8 @@ terraform {
   }
 }
 
-provider "aws" {}
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "suppdeev"
+}
