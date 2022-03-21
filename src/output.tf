a112ab37f52e77d9cd6d69bcadfa607eab12c169
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-03-21 16:42:46 +0200

    Fix TF output

--- src/output.tf
+++ src/output.tf
@@ -1,7 +1,7 @@
 # Output value definitions
 
 output "invoke_url" {
-  value = aws_apigatewayv2_stage.profiles_api_stage.invoke_url
+  value = aws_apigatewayv2_stage.api_stage.invoke_url
 }
 
 output "client_bucket" {
