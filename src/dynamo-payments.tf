31290f2d4da79e211b0a957550d88842faa23ce6
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-03-22 15:51:06 +0200

    DynamoDB resources

---
+++ src/dynamo-payments.tf
@@ -0,0 +1,32 @@
+resource "aws_dynamodb_table" "payments" {
+  name         = "Payments"
+  hash_key     = "id"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "from"
+    type = "S"
+  }
+
+  attribute {
+    name = "to"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "PaymentsFromIndex"
+    hash_key        = "from"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "PaymentsToIndex"
+    hash_key        = "to"
+    projection_type = "ALL"
+  }
+}
