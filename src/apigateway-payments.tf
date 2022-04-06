a29ea4f0bc2117782e988df3945966cdc8c03521
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-04-06 22:19:48 +0300

    Add code for GetPayments

--- src/apigateway-payments.tf
+++ src/apigateway-payments.tf
@@ -48,4 +48,29 @@ resource "aws_lambda_permission" "log_payment_api_permission" {
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*/${trimprefix(aws_apigatewayv2_route.log_payment_route.route_key, "POST /")}"
+
+  # GET PAYMENTS
+resource "aws_apigatewayv2_integration" "log_payment_integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+
+  connection_type    = "INTERNET"
+  description        = "Log Payment"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.log_payment_lambda.invoke_arn
+
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "get_payments_route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /payments"
+  target    = "integrations/${aws_apigatewayv2_integration.get_payments_integration.id}"
+}
+
+resource "aws_lambda_permission" "get_payments_api_permission" {
+  function_name = aws_lambda_function.get_payments_lambda.function_name
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*/${trimprefix(aws_apigatewayv2_route.get_payments_route.route_key, "GET /")}"
 }
--- src/lambda_layer/environment.rs
+++ src/lambda_layer/environment.rs
@@ -8,3 +8,19 @@ pub fn get_env_variable(var_name: &str) -> String {
 
     return env_variable;
 }
+
+// General
+pub const DOMAIN: &str = "DOMAIN";
+
+// Payments
+pub const PAYMENTS_TABLE_NAME: &str = "PAYMENTS_TABLE_NAME";
+pub const ORDER_ID_INDEX_NAME: &str = "ORDER_ID_INDEX_NAME";
+pub const TO_INDEX_NAME: &str = "TO_INDEX_NAME";
+
+
+// Users
+pub const USERS_TABLE_NAME: &str = "USERS_TABLE_NAME";
+pub const USERS_USERNAME_INDEX: &str = "USERS_USERNAME_INDEX";
+
+// Stripe
+pub const STRIPE_WEBHOOK_SECRET: &str = "STRIPE_WEBHOOK_SECRET";
