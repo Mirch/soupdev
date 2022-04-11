1893ebf9830b7d0094c5e2c0f21f052ef97f0f59
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-04-11 23:31:42 +0300

    Rename token to profile

--- src/client/src/pages/profile.js
+++ src/client/src/pages/profile.js
@@ -9,31 +9,34 @@ import { Payment } from '../components/payment';
 
 export function Profile() {
     let params = useParams();
-    const [token, setToken] = useState(null);
+    const [profile, setProfile] = useState(null);
     useEffect(() => {
-        async function getToken() {
-            const token = await getProfileAsync(params.username);
-            setToken(token);
+        async function getProfile() {
+            const profile = await getProfileAsync(params.username);
+            setProfile(profile);
         }
-        getToken();
-    }, [])
+        getProfile();
+    }, []);
     const [donation, setDonation] = useState(5);
+    const [donor, setDonor] = useState(5);
 
-    if (!token) {
+
+    if (!profile) {
         return <div></div>;
     }
-    let paymentQuery = `/pay?to=${token.username}&donation=${donation}`;
+    let paymentQuery = `/pay?to=${profile.username}&donation=${donation}`;
     return (
         <div className="page-container">
             <div className="profile-container">
-                <h1>{token.username}</h1>
-                <h2>is {token.headline}!</h2>
+                <h1>{profile.username}</h1>
+                <h2>is {profile.headline}!</h2>
                 <hr />
                 <br />
-                <p className="profile-description">{token.description}</p>
+                <p className="profile-description">{profile.description}</p>
                 <div className="donations-container">
                     <form action={api_uri + paymentQuery} method="POST">
-                        <input type="number" value={donation} onChange={event => setDonation(event.target.value)}/>
+                        <input type="text" value={donor} onChange={event => setDonor(event.target.value)} />
+                        <input type="number" value={donation} onChange={event => setDonation(event.target.value)} />
                         <button type="submit">Checkout</button>
                     </form>
                 </div>
