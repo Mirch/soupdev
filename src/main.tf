ed49378a50242a94bab9ffcb8ea244338547253b
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-05-17 13:55:54 +0300

    Remove "stripe" from the main module

--- src/main.tf
+++ src/main.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "hashicorp/archive"
       version = "~> 2.2.0"
     }
-    stripe = {
-      source  = "franckverrot/stripe"
-      version = "1.8.0"
-    }
     null = {
       source  = "hashicorp/null"
       version = "3.1.0"
