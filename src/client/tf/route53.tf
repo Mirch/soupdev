13ea95a78b8339ef8698b06280e2dee4ce43b110
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-06-01 15:41:49 +0300

    Add certificate

--- src/client/tf/route53.tf
+++ src/client/tf/route53.tf
@@ -1,8 +1,21 @@
+resource "aws_acm_certificate" "soupdev" {
+  domain_name       = "soup.dev"
+  validation_method = "DNS"
+}
+
 resource "aws_route53_zone" "soupdev" {
   name = "soup.dev"
 }
 
 resource "aws_route53_record" "root_domain" {
+  for_each = {
+    for dvo in aws_acm_certificate.soupdev.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  
   zone_id = aws_route53_zone.soupdev.zone_id
   name = "soup.dev"
   type = "A"
@@ -12,4 +25,9 @@ resource "aws_route53_record" "root_domain" {
     zone_id = aws_cloudfront_distribution.soupdev_cf_distribution.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_acm_certificate_validation" "soupdev" {
+  certificate_arn         = aws_acm_certificate.soupdev.arn
+  validation_record_fqdns = [for record in aws_route53_record.soupdev : record.fqdn]
 }
